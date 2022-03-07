164d58fb9fcf18cdd9bc7b072c108fa3a216119c
anjalibarnwal31 <anjalibarnwal31@gmail.com>
2022-03-07 21:10:09 +0530

    points functionality added

--- game.py
+++ game.py
@@ -1,5 +1,10 @@
 import random
-print(".......SNAKE/WATER/GUN.......")
+a=".......SNAKE/WATER/GUN......."
+print(a.center(100))
+winning=0
+loosing=0
+tie=0
+invalid_times=0
 def chosen(comp,user):
     r=True
     if comp=="s":
@@ -26,30 +31,61 @@ def chosen(comp,user):
             return r
         else:
             return None
+print("How many turns u want to bet! :-")
+n=int(input())
+for _ in range(n):
+    print("Choose your option")
+    print("'s' for snake")
+    print("'g' for gun")
+    print("'w' for water:")
+    user=input()
+    compScore=random.randint(1,3)
+    if compScore==1:
+        comp= "s"
+    elif compScore==2:
+        comp= "g"
+    elif compScore==3:
+        comp="w"
+    print("computer chose :"+comp)
+    if user=="s" or user=="g" or user=="w":
+         a= chosen(comp,user)
+         if a==None:
+            print("Game tie🤗")
+            print(".....................................")
+            tie+=1
+         elif a==True:
+            print("U win🥳🥳")
+            print(".....................................")
+            winning+=1
+         elif a==False:
+            print("U lose!🙄")
+            print(".....................................")
+            loosing+=1
+    else:
+         print("Chosen option not available")
+         print("...................")
+print("************************************************************************")
+print("YOUR RESULT:-")
+if winning>loosing:
+    print("Congratulations you won!!!")
+    print("your points:- "+str(winning))
+    print("computer points:- "+str(loosing))
 
-print("Choose your option")
-print("'s' for snake")
-print("'g' for gun")
-print("'w' for water:")
-user=input()
-compScore=random.randint(1,3)
-if compScore==1:
-    comp= "s"
-elif compScore==2:
-    comp= "g"
-elif compScore==3:
-    comp="w"
-print("computer chose :"+comp)
-if user=="s" or user=="g" or user=="w":
-     a= chosen(comp,user)
-     if a==None:
-        print("Game tie🤗")
-     elif a==True:
-        print("U win🥳🥳")
-     elif a==False:
-        print("U lose!🙄")
+
+elif loosing > winning:
+    print(" you lose!!!")
+    print("your points:- " + str(winning))
+    print("computer points:- " + str(loosing))
+
+
 else:
-     print("input is invalid")
+    print("Game tie")
+    print("your points:- " + str(winning))
+    print("computer points:- " + str(loosing))
+
+
+
+
 
 
 
